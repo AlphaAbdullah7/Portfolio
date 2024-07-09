98a30e62bca85326c5714c27e47ccf4e0e2795e3
AlphaAbdullah7 <muhammadabdullah932747@gmail.com>
2024-07-09 23:13:23 +0500

    FC

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import React from "react";
-import Logo from "../../../public/Logo.jpg";
 
 const Footer = () => {
 	return (
@@ -9,7 +8,8 @@ const Footer = () => {
 				<span>
 					<Image
 						width={50}
-						src={Logo}
+						height={50}
+						src='/Logo.jpg'
 						alt='Logo'
 					/>
 				</span>
